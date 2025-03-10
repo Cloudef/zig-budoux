8fa81f60ddc5b2220c30b86c231b54f7962e92c9
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2025-03-10 17:39:42 +0900

    update zig

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       # Check the flake.nix in zig-flake project for more options:
       # <https://github.com/Cloudef/mach-flake/blob/master/flake.nix>
       env = zig2nix.outputs.zig-env.${system} {
-        zig = zig2nix.outputs.packages.${system}.zig.master.bin;
+        zig = zig2nix.outputs.packages.${system}.zig-master;
       };
     in rec {
       # nix build .
